7b175c53825789378b4a3737ef6d2ba9f033a404
Aashish Jolly <aajolly@ajolly-mbp.local>
2023-05-25 12:55:01 +1000

    updated aviatrix provider config

--- providers.tf
+++ providers.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/aws"
       version = "~> 4.0"
     }
+		aviatrix = {
+      source = "AviatrixSystems/aviatrix"
+      version = "3.1.0"
+    }
   }
 }
 provider "aws" {
