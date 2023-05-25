3078a34e48e01e135b008470abda3e2df001971b
Aashish Jolly <aajolly@ajolly-mbp.local>
2023-05-25 15:23:01 +1000

    updated region name

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -409,7 +409,7 @@ provider "helm" {
     host = aws_eks_cluster.eks.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority.0.data)
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1"
       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
       command     = "aws"
     }
