c9c312c7d12548ab8ed8df96632bf0144e01265f
Aashish Jolly <aajolly@ajolly-mbp.local>
2023-05-30 21:50:14 +1000

    add helm_release

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -404,57 +404,57 @@ resource "kubernetes_service_account" "aws_load_balancer_controller_sa" {
   }
 }
 
-# provider "helm" {
-#   kubernetes {
-#     host = aws_eks_cluster.eks.endpoint
-#     cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority.0.data)
-#     exec {
-#       api_version = "client.authentication.k8s.io/v1"
-#       args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-#       command     = "aws"
-#     }
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host = aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
+  }
+}
 
-# resource "helm_release" "awslbc" {
-#   name = "aws-load-balancer-controller"
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "aws-load-balancer-controller"
-#   namespace = "kube-system"
-#   depends_on = [
-#     kubernetes_service_account.aws_load_balancer_controller_sa,
-#     aws_eks_cluster.eks
-#   ]
-#   set {
-#     name = "region"
-#     value = data.aws_region.current.name
-#   }
-#   set {
-#     name  = "serviceAccount.name"
-#     value = "aws-load-balancer-controller"
-#   }
-#   set {
-#     name = "clusterName"
-#     value = var.cluster_name
-#   }
-#   set {
-#     name  = "image.repository"
-#     value = "602401143452.dkr.ecr.${var.region}.amazonaws.com/amazon/aws-load-balancer-controller"
-#   }
-#   set {
-#     name  = "serviceAccount.create"
-#     value = "false"
-#   }
-#   set {
-#     name = "enable-shield"
-#     value = "false"
-#   }
-#   set {
-#     name = "enable-waf"
-#     value = "false"
-#   }
-#   set {
-#     name = "enable-wafv2"
-#     value = "false"
-#   }
-# }
+resource "helm_release" "awslbc" {
+  name = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace = "kube-system"
+  depends_on = [
+    kubernetes_service_account.aws_load_balancer_controller_sa,
+    aws_eks_cluster.eks
+  ]
+  set {
+    name = "region"
+    value = data.aws_region.current.name
+  }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+  set {
+    name = "clusterName"
+    value = var.cluster_name
+  }
+  set {
+    name  = "image.repository"
+    value = "602401143452.dkr.ecr.${var.region}.amazonaws.com/amazon/aws-load-balancer-controller"
+  }
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+  set {
+    name = "enable-shield"
+    value = "false"
+  }
+  set {
+    name = "enable-waf"
+    value = "false"
+  }
+  set {
+    name = "enable-wafv2"
+    value = "false"
+  }
+}
