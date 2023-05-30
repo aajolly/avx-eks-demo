8e7f912f25e18c41c845f01227f15b5d989408c0
Aashish Jolly <aajolly@ajolly-mbp.local>
2023-05-30 22:00:51 +1000

    updated local-exec command

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -373,7 +373,7 @@ resource "null_resource" "merge_kubeconfig" {
     command = <<EOT
       # set -e
       echo 'Applying Auth ConfigMap with kubectl...'
-      aws eks wait cluster-active --name '${aws_eks_cluster.eks.name}-${data.aws_region.current.name}'
+      aws eks wait cluster-active --name '${aws_eks_cluster.eks.name}'
       aws eks update-kubeconfig --name '${aws_eks_cluster.eks.name}' --alias '${aws_eks_cluster.eks.name}-${data.aws_region.current.name}' --region=${data.aws_region.current.name}
     EOT
   }
