a3ef0611d3a0a76716826fc47c866d6f6406099b
Aashish Jolly <aajolly@ajolly-mbp.local>
2023-06-01 14:58:48 +1000

    add shell script

---
+++ k8s/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+REGION=$1
+AWS_ACCOUNT_ID=$2
+echo "## Installing Docker"
+sudo yum update -y
+sudo amazon-linux-extras install docker
+
+echo "Pull Images"
+docker pull aajolly/nyancat:latest
+docker pull erjosito/whereami:1.3
+
+echo "## Login to ECR"
+aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
+
+echo "## Tag Images"
+docker tag nyancat:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/nyancat:latest
+docker tag whereami:1.3 ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/whereami:latest
+
+echo "## Push Images"
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/nyancat:latest
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/whereami:latest
