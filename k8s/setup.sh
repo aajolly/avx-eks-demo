6c3f2a3e351f4ee4b82a1e9be1541a265d227f46
Aashish Jolly <aajolly@ajolly-mbp.local>
2023-06-06 12:48:06 +1000

    updated docker tags in shell script

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -13,8 +13,8 @@ echo "## Login to ECR"
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
 
 echo "## Tag Images"
-docker tag nyancat:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/nyancat:latest
-docker tag whereami:1.3 ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/whereami:latest
+docker tag aajolly/nyancat:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/nyancat:latest
+docker tag erjosito/whereami:1.3 ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/whereami:latest
 
 echo "## Push Images"
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/nyancat:latest
